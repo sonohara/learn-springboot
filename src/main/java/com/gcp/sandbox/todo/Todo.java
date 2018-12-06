fc20bc429282bfdcc7c5ceb00421f429ac1a02e1
sonohara <5350767+sonohara@users.noreply.github.com>
2018-12-06 21:31:30 +0900

    add: Add Bean Validation

--- src/main/java/com/gcp/sandbox/todo/Todo.java
+++ src/main/java/com/gcp/sandbox/todo/Todo.java
@@ -4,13 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Todo {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String body;
 
     public Todo() {
--- src/main/java/com/gcp/sandbox/todo/TodoController.java
+++ src/main/java/com/gcp/sandbox/todo/TodoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,18 +24,24 @@ public class TodoController {
     }
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, @ModelAttribute Todo todo) {
         List<Todo> todos = this.todoService.findAll();
         model.addAttribute("title", "Hello Thymeleaf !");
         model.addAttribute("todos", todos);
+        model.addAttribute("todo", todo);
         return "todo/index";
     }
 
     @PostMapping
     public String create(
-        @ModelAttribute Todo todo,
+        @ModelAttribute @Validated Todo todo,
+        BindingResult result,
+        Model model,
         RedirectAttributes redirectAttributes
     ) {
+        if (result.hasErrors()) {
+            return this.index(model, todo);
+        }
         this.todoService.save(todo);
         redirectAttributes.addFlashAttribute("message", "Successful Created !");
         return "redirect:/todo";
